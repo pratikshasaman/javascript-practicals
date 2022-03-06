d4b680067de8e620a283082b1bd4819707d64fc2
pratikshasamane <pratikshas@bsf.io>
2022-03-06 23:18:58 +0530

    Css, title and icon added

--- Modal-popup/script.js
+++ Modal-popup/script.js
@@ -1,14 +1,15 @@
 "use strict";
 const btnMain = document.querySelector(".main");
-const btnClose = document.querySelector(".close");
+const overflow = document.querySelector(".overflow");
+
+const btnClose = document.querySelector(".top-section .close-icon");
 const popupMain = document.querySelector(".popup-main");
 const btnClass = document.querySelectorAll(".btn");
 for (let i = 0; i < btnClass.length; i++) {
-  // console.log(btnClass[i]);
   btnClass[i].addEventListener("click", function () {
     popupMain.classList.remove("hidden");
   });
-  // console.log(btnClose[i]);
+
   btnClose.addEventListener("click", function () {
     popupMain.classList.add("hidden");
   });
