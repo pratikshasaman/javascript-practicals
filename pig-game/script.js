2fc38f03c3ad1ea040897ad822c1d93c663f4c0f
pratikshasamane <pratikshas@bsf.io>
2022-03-08 00:30:14 +0530

    added reset functionality

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+let scores, currentScore, activePlayer;
 const score0 = document.querySelector("#score--0");
 const score1 = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
@@ -9,12 +9,19 @@ const current0 = document.querySelector("#current--0");
 const current1 = document.querySelector("#current--1");
 const player0 = document.querySelector(".player--0");
 const player1 = document.querySelector(".player--1");
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-score0.textContent = 0;
-score1.textContent = 0;
-diceEl.classList.add("hidden");
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  score0.textContent = 0;
+  score1.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0.classList.add("player--active");
+  player1.classList.remove("player--active");
+};
+
+init();
 
 const switchUser = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -43,3 +50,5 @@ btnRoll.addEventListener("click", function () {
     switchUser();
   }
 });
+
+btnNew.addEventListener("click", init);
